805e54f77aeb084fce72ef3b71cb874fbea5c0de
xianfei <xianfei@bupt.cn>
2026-02-25 01:33:47 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  assetPrefix: isProd ? "/flappy/" : "",
+  assetPrefix: isProd ? "/" : "",
   images: {
     unoptimized: true,
   },
